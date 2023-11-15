f3ae3e220115dce433d21597d496bb940a76b3a1
Andrew Strange <andrew@foodbomb.com.au>
2023-11-15 23:18:44 +1100

    done i thing

--- src/teamsSheetMaker.js
+++ src/teamsSheetMaker.js
@@ -5,31 +5,38 @@ import React, {
     useState,
     StrictMode,
   } from 'react';
+import moment from 'moment'
 import { AgGridReact } from 'ag-grid-react';
 import 'ag-grid-enterprise';
 import 'ag-grid-community/styles/ag-grid.css';
 import { exportMultipleSheetsAsExcel } from 'ag-grid-enterprise';
 
+const delay = ms => new Promise(
+  resolve => setTimeout(resolve, ms)
+);
+
 function TeamsSheetMaker() {
     const shiftsGridRef = useRef();
     const containerStyle = useMemo(() => ({ width: '100%', height: '100%' }), []);
     const gridStyle = useMemo(() => ({ height: '100%', width: '100%' }), []);
-    const [shiftsRowData, setRowData] = useState([
-      {
-        fullName: 'Andrew Strange',
-        email: 'andrew.strange@uts.edu.au',
-        group: '',
-        startDate: '11/23/2023',
-        startTime: '08:00',
-        endDate: '11/23/2023',
-        endTime: '17:00',
-        themeColor: '7. Gray',
-        customLabel: 'customLabel',
-        unpaidBreak: '',
-        notes: 'notes',
-        shared: '2. Not Shared',
-      },
-    ]);
+    const [shiftsRowData, setShiftsRowData] = useState([])
+
+    // const [shiftsRowData, setRowData] = useState([
+    //   {
+    //     fullName: 'Andrew Strange',
+    //     email: 'andrew.strange@uts.edu.au',
+    //     group: '',
+    //     startDate: '11/23/2023',
+    //     startTime: '08:00',
+    //     endDate: '11/23/2023',
+    //     endTime: '17:00',
+    //     themeColor: '7. Gray',
+    //     customLabel: 'customLabel',
+    //     unpaidBreak: '',
+    //     notes: 'notes',
+    //     shared: '2. Not Shared',
+    //   },
+    // ]);
     const [shiftsColumnDefs, setColumnDefs] = useState([
       { headerName: 'Member', field: 'fullName' },
       { headerName: 'Work Email', field: 'email'},
@@ -45,25 +52,55 @@ function TeamsSheetMaker() {
       { headerName: 'Shared', field: 'shared'}
     ]);
     
-    const onBtExport = useCallback(() => {
-        let spreadsheets = [];
-        spreadsheets.push(
+    const updateData = async event => {
+
+      const teamsData = JSON.parse(localStorage.getItem('teamsCSV'))
+        let tempData = []
+        for (let i = 1; i < teamsData.length; i++) {
+          tempData.push({
+            fullName: teamsData[i][0],
+            email: teamsData[i][1],
+            group: teamsData[i][2],
+            startDate: teamsData[i][3],
+            startTime: teamsData[i][4],
+            endDate: teamsData[i][5],
+            endTime: teamsData[i][6],
+            themeColor: teamsData[i][7],
+            customLabel: teamsData[i][8],
+            unpaidBreak: teamsData[i][9],
+            notes: teamsData[i][10],
+            shared: teamsData[i][11],
+          })
+        }
+        setShiftsRowData(tempData)
+        await delay(1000);
+        exportFile()
+      }
+
+      const exportFile = useCallback(() => {
+        //console.log("shiftsRowData", shiftsRowData)
+          let spreadsheets = [];
+          spreadsheets.push(
             shiftsGridRef.current.api.getSheetDataForExcel({ sheetName: 'Shifts' }),
-        );
-        console.log("spreadsheets". spreadsheets)
-        exportMultipleSheetsAsExcel({
-            data: spreadsheets,
-            fileName: 'tables.xlsx',
-        });
-    })
+          );
+          console.log("spreadsheets". spreadsheets)
+          exportMultipleSheetsAsExcel({
+              data: spreadsheets,
+              fileName: "MS Teams Shifts Export " + moment().format('MMMM Do YYYY, h:mm:ss a') + ".xlsx",
+          });
+      })
 
     return (
-        <div style={containerStyle}>
+      <div style={containerStyle}>
         <div className="example-wrapper">
           <div className="example-header">
-            <button onClick={onBtExport} style={{ fontWeight: 'bold' }}>
-              Export to Excel
+            <button onClick={updateData} style={{ fontWeight: 'bold', fontSize: 16 }}>
+              Export MS Teams Excel
             </button>
+          </div>
+
+          <div style={{padding: 40}}>
+
           </div>
   
           <div style={gridStyle} className="ag-theme-alpine">
